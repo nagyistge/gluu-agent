07521af37e40741a119ad8c00fc0bb91224e905a
Isman Firmansyah <isman.firmansyah@gmail.com>
2015-10-14 04:14:53 +0700

    revamp gluu-agent CLI

--- gluuagent/cli.py
+++ gluuagent/cli.py
@@ -10,7 +10,9 @@ from .tasks import RecoveryTask
 from .utils import get_logger
 
 
-@click.group()
+@click.group(context_settings={
+    "help_option_names": ["-h", "--help"],
+})
 def main():
     pass
 
@@ -18,17 +20,19 @@ def main():
 @main.command()
 @click.option(
     "--database",
-    default="/var/lib/gluu-cluster/db.json",
-    help="Path to database file (default to /var/lib/gluu-cluster/db.json)",
+    default="/var/lib/gluu-cluster/db/db.json",
+    help="Path to database file (default to /var/lib/gluu-cluster/db/db.json)",
     metavar="<database>",
     )
 @click.option(
     "--logfile",
     default="/var/log/gluuagent-recover.log",
-    help="Path to database file (default to /var/log/gluuagent-recover.log)",
+    help="Path to log file (default to /var/log/gluuagent-recover.log)",
     metavar="<logfile>",
     )
 def recover(database, logfile):
+    """Run recovery process.
+    """
     db = Database(database)
     logger = get_logger(logfile)
     task = RecoveryTask(db, logger)
